ab079010320441fe74b1a11c6a52d4f264661629
jhoonpaik <jhoon.paik@gmail.com>
2025-02-02 21:22:32 +0900

    kwargs op upload

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -2,7 +2,23 @@ def get_sftp():
     print('sftp 작업을 시작합니다.')
 
 
+# *args 실습
 def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
     print(f'기타옵션들: {args}')
+
+# **kwargs 실습
+def regist2(name, sex, *args, **kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타옵션들: {args}')
+
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+
+    if email:
+        print(email)
+
+    if phone:
+        print(phone)
