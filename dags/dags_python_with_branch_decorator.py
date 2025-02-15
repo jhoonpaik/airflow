5e47e32c2e7bfe8d5b6b154fb3bb736e4f20691d
jhoonpaik <jhoon.paik@gmail.com>
2025-02-15 11:20:43 +0900

    dag_python_with_branch_deco dags upload

---
+++ dags/dags_python_with_branch_decorator.py
@@ -0,0 +1,49 @@
+from airflow import DAG
+from datetime import datetime
+import random
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+
+
+with DAG(
+    dag_id = "dags_branch_python_operator",
+    start_date = datetime(2023, 4, 1),
+    schedule=None,
+    catchup=False
+) as dag:
+    @task.branch(task_id='python_branch_task')
+    def select_random():
+        import random
+        item_lst = ['A', 'B', 'C']
+        
+        selected_item = random.choice(item_lst)
+        if selected_item == 'A':
+            return 'task_a' # 리턴값이 후행 테스크
+        elif selected_item in ['B','C']:
+            return ['task_b', 'task_c'] # 리턴값이 여러 개면 리스트 형태로
+
+
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+    
+    select_random() >> [task_a, task_b, task_c]
+    # select_random 함수가 실행되면 @task.branch 기능을 통해 해당 함수가 python_branch_task task로 변환되어 수행됨.
